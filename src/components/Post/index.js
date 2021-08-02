b03c4425ae86a422d6adcf14ed04ad2353f68e78
Násser Yousef Ali <nasser.yousef.unb@gmail.com>
2021-08-02 04:00:09 -0300

    Updated props in the components.

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,8 +1,24 @@
 
 
-const Post = () => {
+const Post = (props) => {
     return (
-        <h1>Posts</h1>
+        <div className="post mx-2">
+            <div className="img-post"></div>
+            <h5>{props.subtitle}</h5>
+            <h3>{props.title}</h3>
+            <p className="mt-1">
+                {props.children}
+            </p>
+            <div className="flex pt-2">
+                <div className="img-profile"></div>
+                <div className="desc-profile ml-2">
+                    <h6 className="color-blue">Fulano de tal</h6>
+                    <p>
+                        Aug 2, 2020 - 8 min read
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
 
